2e5c82997ead0c7c49114a6184e13b82d1bc9582
Julien Montel <julien.montel@sfr.fr>
2018-07-09 10:15:54 +0200

    Add route for remove player

--- src/App/Controller/Donjon/PlayerCardController.php
+++ src/App/Controller/Donjon/PlayerCardController.php
@@ -15,40 +15,51 @@ use Symfony\Component\Routing\Annotation\Route;
 class PlayerCardController extends Controller
 {
     /**
-     * @param Request $request
-     * @param int     $id
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @param int $id
      *
-     * @return RedirectResponse|Response
+     * @return Response
      *
      * @Route("fiche-joueur/{id}", requirements={"\d+"}, name="playerCard")
      */
-    public function playerCardAction(Request $request, EntityManagerInterface $em, int $id)
+    public function playerCardAction(int $id): Response
     {
         $player = $this->getDoctrine()->getRepository(Player::class)->find($id);
 
-        $form = $this->get('form.factory')->create();
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $kingdomResources = $em->getRepository(KingdomResource::class)->findByKingdom($player->getKingdom());
-
-            foreach ($kingdomResources as $kingdomResource) {
-                $em->remove($kingdomResource);
-            }
-            $em->remove($player);
-            $em->flush();
-
-            $this->addflash('notice', 'Le joueur a bien été supprimé !');
-
-            return $this->redirectToRoute('donjon');
-        }
-
-        $messages = $em->getRepository(Message::class)->findBySender($player);
+        $messages = $this->em->getRepository(Message::class)->findBySender($player);
 
         return $this->render('Donjon/player_card.html.twig', [
             'player' => $player,
-            'messages' => $messages,
-            'form' => $form->createView(),
+            'messages' => $messages
         ]);
+    }
+
+    /**
+     * @return RedirectResponse
+     *
+     * @Route("suppression-joueur/{id}", requirements={"\d+"}, name="remove-player")
+     */
+    public function removePlayer(int $id): RedirectResponse
+    {
+        $player = $this->getDoctrine()->getRepository(Player::class)->find($id);
+        $kingdomResources = $this->em->getRepository(KingdomResource::class)->findByKingdom($player->getKingdom());
+
+        foreach ($kingdomResources as $kingdomResource) {
+            $this->em->remove($kingdomResource);
+        }
+        $this->em->remove($player);
+        $this->em->flush();
+
+        $this->addflash('notice', 'Le joueur a bien été supprimé !');
+        return $this->redirectToRoute('donjon');
     }
 }
